5e9bb2620fd6807e64df1d8f46f5888ffd6263df
Mahroon_Nisha <nisha68259@gmail.com>
2023-03-16 20:12:15 -0400

    Midterm

--- app/src/main/java/com/example/unitconversion/MainActivity.kt
+++ app/src/main/java/com/example/unitconversion/MainActivity.kt
@@ -11,65 +11,55 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding =ActivityMainBinding.inflate(layoutInflater)
-
         setContentView(binding.root)
         binding.convert.setOnClickListener {
-            val context: Context=applicationContext
-            val text:CharSequence = "Seems like your selected option don't have any conversion available."
-            val duration = Toast.LENGTH_SHORT
-            val toast = Toast.makeText(context,text,duration)
-
-            if (binding.spinner1.selectedItem=="Kilometer" && binding.spinner2.selectedItem=="Meter" ){
-                binding.output.text= (binding.input.editableText.toString().toDouble()*1000.0).toString()
-            }
-            else if (binding.spinner1.selectedItem=="Kilometer" && binding.spinner2.selectedItem=="Centimeter" ){
-                binding.output.text= (binding.input.editableText.toString().toDouble()*100000.0).toString()
-            }
-            else if (binding.spinner1.selectedItem=="Centimeter" && binding.spinner2.selectedItem=="Meter" ){
-                binding.output.text= (binding.input.editableText.toString().toDouble()*0.01).toString()
-            }
-            else if (binding.spinner1.selectedItem=="Centimeter" && binding.spinner2.selectedItem=="Kilometer" ){
-                binding.output.text= (binding.input.editableText.toString().toDouble()*0.00001).toString()
-            }
-            else if (binding.spinner1.selectedItem=="Meter" && binding.spinner2.selectedItem=="Kilometer" ){
-                binding.output.text= (binding.input.editableText.toString().toDouble()*0.001).toString()
-            }
-            else if (binding.spinner1.selectedItem=="Meter" && binding.spinner2.selectedItem=="Centimeter" ){
-                binding.output.text= (binding.input.editableText.toString().toDouble()*100.0).toString()
-            }
-            else if (binding.spinner1.selectedItem=="Gram" && binding.spinner2.selectedItem=="Kilogram" ){
-                binding.output.text= (binding.input.editableText.toString().toDouble()*0.001).toString()
-            }
-            else if (binding.spinner1.selectedItem=="Gram" && binding.spinner2.selectedItem=="Pound" ){
-                binding.output.text= (binding.input.editableText.toString().toDouble()*0.0022).toString()
-            }
-            else if (binding.spinner1.selectedItem=="Kilogram" && binding.spinner2.selectedItem=="Gram" ){
-                binding.output.text= (binding.input.editableText.toString().toDouble()*1000.0).toString()
-            }
-            else if (binding.spinner1.selectedItem=="Kilogram" && binding.spinner2.selectedItem=="Pound" ){
-                binding.output.text= (binding.input.editableText.toString().toDouble()*2.2).toString()
-            }
-            else if (binding.spinner1.selectedItem=="Pound" && binding.spinner2.selectedItem=="Gram" ){
-                binding.output.text= (binding.input.editableText.toString().toDouble()*453.592).toString()
-            }
-            else if (binding.spinner1.selectedItem=="Pound" && binding.spinner2.selectedItem=="Kilogram" ){
-                binding.output.text= (binding.input.editableText.toString().toDouble()*0.453592).toString()
-            }
-            else if (binding.spinner1.selectedItem=="Litre" && binding.spinner2.selectedItem=="Millilitre" ){
-                binding.output.text= (binding.input.editableText.toString().toDouble()*1000.0).toString()
-            }
-            else if (binding.spinner1.selectedItem=="Millilitre" && binding.spinner2.selectedItem=="Litre" ){
-                binding.output.text= (binding.input.editableText.toString().toDouble()*0.001).toString()
-            }
-            else if (binding.spinner1.selectedItem=="Kelvin" && binding.spinner2.selectedItem=="Celsius" ){
-                binding.output.text= (binding.input.editableText.toString().toDouble()-273.15).toString()
-            }
-            else if (binding.spinner1.selectedItem=="Celsius" && binding.spinner2.selectedItem=="Kelvin" ){
-                binding.output.text= (binding.input.editableText.toString().toDouble()+273.15).toString()
-            }
-            else{
-                toast.show()
-            }
+            convert()
         }
+        val context: Context = applicationContext
+        val text: CharSequence =
+            "Welcome to Conversion Calculator"
+        val duration = Toast.LENGTH_SHORT
+        val toast = Toast.makeText(context, text, duration)
+        binding.main.setOnClickListener {
+            toast.show()
+        }
+    }
+    private fun convert() {
+        val input = binding.input.text.toString().toDoubleOrNull() ?: return
+        val from = binding.spinner1.selectedItem.toString()
+        val to = binding.spinner2.selectedItem.toString()
+        val result = when {
+            from == "Kilometer" && to == "Meter" -> input * 1000.0
+            from == "Kilometer" && to == "Centimeter" -> input * 100000.0
+            from == "Centimeter" && to == "Meter" -> input * 0.01
+            from == "Centimeter" && to == "Kilometer" -> input * 0.00001
+            from == "Meter" && to == "Kilometer" -> input * 0.001
+            from == "Meter" && to == "Centimeter" -> input * 100.0
+            from == "Gram" && to == "Kilogram" -> input * 0.001
+            from == "Gram" && to == "Pound" -> input * 0.0022
+            from == "Kilogram" && to == "Gram" -> input * 1000.0
+            from == "Kilogram" && to == "Pound" -> input * 2.2
+            from == "Pound" && to == "Gram" -> input * 453.592
+            from == "Pound" && to == "Kilogram" -> input * 0.453592
+            from == "Litre" && to == "Millilitre" -> input * 1000.0
+            from == "Millilitre" && to == "Litre" -> input * 0.001
+            from == "Kelvin" && to == "Celsius" -> input - 273.15
+            from == "Celsius" && to == "Kelvin" -> input + 273.15
+            else -> null
+        }
+        if (result != null) {
+            binding.output.text = result.toString()
+        } else {
+            showToast()
+        }
+    }
+
+    private fun showToast() {
+        val context: Context = applicationContext
+        val text: CharSequence =
+            "Seems like your selected option don't have any conversion available."
+        val duration = Toast.LENGTH_SHORT
+        val toast = Toast.makeText(context, text, duration)
+        toast.show()
     }
 }
